1a782501e86b9785a0c280633736cad2d3f87976
devmarka <mi6softlab@gmail.com>
2020-06-14 09:21:22 -0400

    ---api--

---
+++ core/api/bbbapi.go
@@ -0,0 +1,37 @@
+package api
+
+func BBBCreateEvent(eventid string) {
+
+}
+
+func BBBEndEvent(eventid string) {
+
+}
+
+func BBBGetEventURL(eventid string, username string, password string) string {
+	return ""
+}
+
+func BBBEventRunning(eventid string) bool {
+	return true
+}
+
+func BBBEventRecording(eventid string) {
+
+}
+
+func BBBEventRecordings([]string) []string {
+
+}
+
+func BBBEventDeleteRecording(recorid string) {
+
+}
+
+func BBBBuildURL(requestType string, args map[string]interface{}) string {
+	return ""
+}
+
+func BBBBuildDownloadURL(recordid string) {
+
+}
